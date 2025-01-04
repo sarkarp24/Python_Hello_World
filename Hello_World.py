2e5c5436f4e04749a49568b4f14123c81d5c9934
sarkarp24 <sarkar.prodyot@gmail.com>
2025-01-03 21:37:56 -0800

    01032025

--- Hello_World.py
+++ Hello_World.py
@@ -11,4 +11,6 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
