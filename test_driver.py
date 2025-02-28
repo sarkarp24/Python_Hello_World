2f38f97c45d15a09600ec2037093ae97729fa2fa
sarkarp24 <sarkar.prodyot@gmail.com>
2025-02-28 07:24:24 -0800

    Update test_driver.py

--- test_driver.py
+++ test_driver.py
@@ -1,3 +1,4 @@
+'''
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -10,3 +11,24 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://google.com/")
 print(driver.title)
 driver.quit()
+'''
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import tempfile
+
+options = Options()
+user_data_dir = tempfile.mkdtemp() #Creates a temporary directory.
+options.add_argument(f"--user-data-dir={user_data_dir}")
+
+service = Service(ChromeDriverManager().install())
+options.add_argument('--headless')
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://google.com/")
+print(driver.title)
+
+driver.quit()
+import shutil
+shutil.rmtree(user_data_dir) #removes the temp directory.
